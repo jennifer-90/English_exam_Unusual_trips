d6c28201ca9a2edc8da521173526aaecd9a6ec73
unknown <jennifer.bastin.90@gmail.com>
2023-04-21 11:11:23 +0200

    21.04.23

--- app/login.php
+++ app/login.php
@@ -1,1 +1,21 @@
 <?php
+
+
+/**************** -- On test si ça fonctione -- ************
+ *                                                         *
+ *    if(!empty($_POST['login']) && ($_POST['pwd'])){      *
+ *       foreach($_POST as $key=>$values){                 *
+ *           echo $key.' =>'.$values;                      *
+ *      }                                                  *
+ *      }else{                                             *
+ *       echo 'Veuillez vous connecté';                    *
+ *   }                                                     *
+ *                                                         *
+ ***********************************************************/
+
+if(!empty($_POST['login']) && ($_POST['pwd'])) {
+
+
+
+
+}
